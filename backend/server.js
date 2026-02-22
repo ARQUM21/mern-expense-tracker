417763720e3bd9dec345d2c3b11d1818a575da1a
Muhammad Arqum Tariq <154671211+ARQUM21@users.noreply.github.com>
2026-02-22 18:06:58 +0500

    Export app instance from server.js
    Added module.exports to export the app instance.

--- backend/server.js
+++ backend/server.js
@@ -37,3 +37,5 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server running on Port ${PORT}`));
+
+module.exports = app;
