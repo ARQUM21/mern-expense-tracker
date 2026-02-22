eb930f31008ea65fdd43da249e00ccdb313b525a
Muhammad Arqum Tariq <154671211+ARQUM21@users.noreply.github.com>
2026-02-22 22:49:59 +0500

    Update apiPath.js

--- frontend/expense-tracker/src/utils/apiPath.js
+++ frontend/expense-tracker/src/utils/apiPath.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://mern-expense-tracker-backend-ten.vercel.app";
+export const BASE_URL = "https://mern-expense-tracker-backend-gold.vercel.app";
 
 // utils/apiPaths.js
 export const API_PATHS = {
