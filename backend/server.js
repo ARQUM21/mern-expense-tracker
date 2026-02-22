41e9af19b1126bf278476bb058deb2dd2ceb535d
Muhammad Arqum Tariq <154671211+ARQUM21@users.noreply.github.com>
2026-02-22 22:29:25 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -30,8 +30,7 @@ app.use('/api/v1/expense', expenseRoutes);
 app.use('/api/v1/dashboard', dashboardRoutes);
 
 
-// serve uploads folder
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.get('/', (req, res) => res.send('API is working'));
 
 
 const PORT = process.env.PORT || 5000;
